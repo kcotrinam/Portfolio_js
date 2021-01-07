c38daf3125980b7965a129da28782c4b3c799617
Kevin Cotrina <kcotrinam92@gmail.com>
2021-01-07 18:29:09 -0500

    Add burger button with motion

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const burgerBtn = document.querySelector('#burger-btn');
+const dashList = [...document.querySelectorAll('#burger-btn span')]
+const menu = document.querySelector('#navbar-list')
+
+burgerBtn.addEventListener('click', () => {
+  dashList.map((el,idx) => {
+    switch (idx) {
+      case 0:
+        el.classList.toggle('rotate-first')
+        break;
+      case 1:
+        el.classList.toggle('hide')
+        break;
+      default:
+        el.classList.toggle('rotate-second')
+        break;
+    }
+    menu.classList.toggle('show-menu')
+  })
+})
