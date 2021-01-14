d600ca6a060f45e5d2112b5838320806dadca6e8
Kevin Cotrina <kcotrinam92@gmail.com>
2021-01-14 02:52:01 -0500

    Add background to the articles in reponsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,8 +116,18 @@ const fillArticleTemplate = (title, lg, img) => {
   const templateInfo = copyTemplate.querySelector('.article__info')
   const [upperImg, lowerImg] = [...copyTemplate.querySelectorAll('.article__img')]
 
-  upperImg.style.background = `url(assets/img/${img.upper}) top/cover`;
-  lowerImg.style.background = `url(assets/img/${img.lower}) top/cover`;
+  // upperImg.style.background = `url(assets/img/${img.upper}) top/cover`;
+  // lowerImg.style.background = `url(assets/img/${img.lower}) top/cover`;
+  upperImg.style.backgroundImage = `linear-gradient(180deg,
+                                    rgba(0,0,0,0.75) 13%,
+                                    rgba(5,5,22,0.5746038732394366) 50%,
+                                    rgba(0,0,0,1) 100%),
+                                    url(assets/img/${img.upper})`;
+  lowerImg.style.backgroundImage = `linear-gradient(180deg,
+                                    rgba(0,0,0,0.75) 13%,
+                                    rgba(5,5,22,0.5746038732394366) 50%,
+                                    rgba(0,0,0,0.75) 100%),
+                                    url(assets/img/${img.lower}`;
   templateInfo.style.background = `url(assets/img/${img.main}) center/cover`;
 
   copyTemplate.querySelector('.article__name').textContent = title
